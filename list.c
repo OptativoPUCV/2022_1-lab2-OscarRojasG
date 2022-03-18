215f320133bdeb9edfbadd611138516855b23b0a
OscarRojasG1 <>
2022-03-18 14:41:59 +0000

    new try

--- list.c
+++ list.c
@@ -42,6 +42,10 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
+    if(list->current) {
+      list->current = list->current->next;
+      return list->current;
+    }
     return NULL;
 }
 
